b56b9c82c66472444982b8761a9027f7202a974d
chendexiang <937682896@qq.com>
2020-02-22 11:35:05 +0800

    image_picker的集成，并且拍照获取图片。

--- lib/photo_demo.dart
+++ lib/photo_demo.dart
@@ -1,4 +1,9 @@
+
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class PhotoDemo extends StatefulWidget {
   @override
@@ -6,8 +11,33 @@ class PhotoDemo extends StatefulWidget {
 }
 
 class _PhotoDemoState extends State<PhotoDemo> {
+
+  File _image;
+
+  Future getImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      _image = image;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Image Picker Example'),
+      ),
+      body: Center(
+        child: _image == null
+            ? Text('No image selected.')
+            : Image.file(_image),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Pick Image',
+        child: Icon(Icons.add_a_photo),
+      ),
+    );
   }
 }
