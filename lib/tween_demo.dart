7317adfad0f10f601bf19f1474606a2110dbdcae
chendexiang <937682896@qq.com>
2020-02-26 16:52:41 +0800

    动画。

--- lib/tween_demo.dart
+++ lib/tween_demo.dart
@@ -1,19 +1,78 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter_app/view/my_stateful_widget_appbar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app/utils/toast.dart';
 
-class TweenDemo extends MyStatefulWidgetAppbar {
-
+class TweenDemo extends StatefulWidget {
   @override
-  Widget getBody() {
-    return Text('test');
-  }
-
-  @override
-  String getTitle() {
-
-    return 'Tween例子';
-  }
-
+  _TweenDemoState createState() => _TweenDemoState();
 }
+
+class _TweenDemoState extends State<TweenDemo> with SingleTickerProviderStateMixin{
+
+  AnimationController animationController;
+  Animation animation;
+  AnimationStatus animationStatus;
+  double animationValue;
+  int animationIntValue;
+
+  @override
+  void initState() {
+    super.initState();
+    // vsync时会防止屏幕外动画消耗不必要的资源，可以将stateful对象作为vsync的值。
+    animationController = AnimationController(duration: Duration(seconds: 3),vsync: this);
+    // 这种形式比较特殊
+    animation = Tween<double>(begin: 0.0,end: 300.0).animate(animationController)
+      ..addListener((){
+        setState(() {
+          animationValue = animation.value;
+          //细节：将double转换为int,先转换为String类型，然后转换为int
+          animationIntValue = int.parse(animationValue.toStringAsFixed(0));
+        });
+      })
+      ..addStatusListener((AnimationStatus status){
+        setState(() {
+          animationStatus = status;
+        });
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tween例子',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Tween例子'),
+        ),
+        body: (
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              RaisedButton(
+                child: Text('开启动画'),
+                onPressed: (){
+                  animationController.reset();
+                  animationController.forward();
+                },
+              ),
+              Text('${animationIntValue}'),
+              Text('状态：$animationStatus'),
+              Center(
+               child: Container(
+                 width: animationValue,
+                 height: animationValue,
+                 child: FlutterLogo(),
+               ),
+              )
+            ],
+          )
+        ),
+      ),
+    );
+  }
+}
+
 
--- lib/view/my_stateful_widget_appbar.dart
+++ lib/view/my_stateful_widget_appbar.dart
@@ -3,24 +3,25 @@ import 'package:flutter/material.dart';
 
 /// 封装了StatefulWidget的MaterialApp和Scafolld
 /// 使用的页面重写getTitle和getBody就行了，没有必须都进行重写
-abstract class MyStatefulWidgetAppbar extends StatefulWidget {
+abstract class MyStatefulWidgetAppbar extends StatefulWidget with Params{
 
   MyStatefulWidgetAppbar();
 
-  String getTitle();
-
-  Widget getBody();
-
   @override
-  _MyStatefulWidgetAppbarState createState() => _MyStatefulWidgetAppbarState(getTitle(),getBody());
+  _MyStatefulWidgetAppbarState createState() => _MyStatefulWidgetAppbarState(this);
 }
 
 class _MyStatefulWidgetAppbarState extends State<MyStatefulWidgetAppbar> {
 
-  String _title;
-  Widget _body;
+  Params _params;
 
-  _MyStatefulWidgetAppbarState(this._title,this._body);
+  _MyStatefulWidgetAppbarState(this._params);
+
+
+  @override
+  void initState() {
+    _params.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,11 +30,25 @@ class _MyStatefulWidgetAppbarState extends State<MyStatefulWidgetAppbar> {
       theme: ThemeData(primarySwatch: Colors.blue),
       home: Scaffold(
         appBar: AppBar(
-          title: Text(_title),
+          title: Text(_params.getTitle()),
         ),
-        body: _body,
+        body: _params.getBody(),
       ),
     );
+  }
+}
+
+class Params{
+  String getTitle(){
+    return '例子';
+  }
+
+  Widget getBody(){
+    return Text('test');
+  }
+
+  initState(){
+
   }
 }
 
